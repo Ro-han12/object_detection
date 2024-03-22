b20be33e3d4b2200a8de03256fe4bc7a7bb85afe
rohan <rohannair2939@gmail.com>
2024-03-22 19:38:34 +0530

    data obtained

--- scrap.py
+++ scrap.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-import re
+import urllib.request
 
 def fetch_image_urls(search_url, number_images):
     headers = {
@@ -17,22 +17,21 @@ def fetch_image_urls(search_url, number_images):
                 break
     return image_urls
 
-def persist_image(folder_path, url, counter):
-    try:
-        image_content = requests.get(url).content
-        with open(os.path.join(folder_path, f"image_{counter}.jpg"), 'wb') as f:
-            f.write(image_content)
-        print(f"Image {counter} downloaded successfully")
-    except Exception as e:
-        print(f"Error downloading image {counter}: {e}")
+def download_images(image_urls, target_folder):
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    for i, url in enumerate(image_urls, start=1):
+        file_name = os.path.join(target_folder, f"image_{i}.jpg")
+        try:
+            urllib.request.urlretrieve(url, file_name)
+            print(f"Image {i} downloaded successfully")
+        except Exception as e:
+            print(f"Error downloading image {i}: {e}")
 
 def scrape_images(search_url, number_images):
     image_urls = fetch_image_urls(search_url, number_images)
     target_folder = './scraped_images'
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
-    for i, url in enumerate(image_urls, start=1):
-        persist_image(target_folder, url, i)
+    download_images(image_urls, target_folder)
 
 search_url = 'https://www.google.com/search?sca_esv=781fa1a7ec643214&sca_upv=1&rlz=1C5MACD_enIN1079IN1080&sxsrf=ACQVn09uL7oPlPQsRwSVQmHtwf48LD3MpQ:1711115469601&q=hand+palm&tbm=isch&source=lnms&prmd=isvnmbtz&sa=X&ved=2ahUKEwiXx_DtgYiFAxUnSGcHHRSACJcQ0pQJegQIHRAB&biw=1440&bih=779&dpr=2'
 number_images = 50
