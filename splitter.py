0bd59eb29afa60d1c0ae0854ad14251ee4df55a6
rohan <rohannair2939@gmail.com>
2024-04-11 10:01:48 +0530

    data update

--- splitter.py
+++ splitter.py
@@ -1,2 +1,2 @@
 import splitfolders 
-splitfolders.ratio("scraped_images2",output="output",seed=1337,ratio=(0.8,0.2),group_prefix=None)
+splitfolders.ratio("hand",output="output",seed=1337,ratio=(0.8,0.2),group_prefix=None)
